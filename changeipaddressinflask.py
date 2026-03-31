345b8611479711f284f00861f15ad4083853794c
Y.Chaitanya <yaragallachaitanya@gmail.com>
2026-03-31 12:22:11 +0530

    Add files via upload

---
+++ changeipaddressinflask.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def demo():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(host='192.168.0.105:5000')
+    
